acb33366d68f949651d6eceab4cff76b81710617
paly2 <plangrognet@laposte.net>
2016-09-07 17:31:38 +0200

    Put whitelist into init.lua instead of whitelist.lua To avoid use of global variables

--- init.lua
+++ init.lua
@@ -3,10 +3,8 @@ xban = { MP = minetest.get_modpath(minetest.get_current_modname()) }
 
 dofile(xban.MP.."/serialize.lua")
 
-db = { }
+local db = { }
 db.whitelist = { }
-
-dofile(xban.MP.."/whitelist.lua")
 
 local tempbans = { }
 
@@ -164,6 +162,44 @@ function xban.get_record(player)
 	return record, last_pos
 end
 
+function xban.get_whitelist_entry(player) --> bool, index
+	for index, e in ipairs(db.whitelist) do
+		if e.name == player or e.ip == player then
+			return true, index
+		end
+	end
+	return false
+end
+
+function xban.whitelist_player(player, source) --> bool, err
+	local in_whitelist, index = xban.get_whitelist_entry(player)
+	if in_whitelist then
+		return false, "Player is already in the whitelist."
+	end
+	local entry, index = xban.find_entry(player)
+	if entry then
+		table.remove(db, index)
+	end
+	local e = { name = player }
+	local ip = minetest.get_player_ip(player)
+	if ip then
+		e.ip = ip
+	end
+	table.insert(db.whitelist, e)
+	ACTION("%s adds %s to the whitelist.", source, player)
+	return true
+end
+
+function xban.unwhitelist_player(player, source) --> bool, err
+	local in_whitelist, index =  xban.get_whitelist_entry(player)
+	if not in_whitelist then
+		return false, "Player is not in the whitelist."
+	end
+	table.remove(db.whitelist, index)
+	ACTION("%s removes %s from the whitelist.", source, player)
+	return true
+end
+
 minetest.register_on_prejoinplayer(function(name, ip)
 	local ip_in_whitelist = xban.get_whitelist_entry(ip)
 	if ip_in_whitelist then
@@ -274,6 +310,34 @@ minetest.register_chatcommand("xban_record", {
 		end
 		return true, "Record listed."
 	end,
+})
+
+minetest.register_chatcommand("xban_ignore", {
+	description = "Add a player to the ban whitelist",
+	params = "<player_or_ip>",
+	privs = { ban=true },
+	func = function(name, params)
+		local plname = params:match("%S+")
+		if not plname then
+			return false, "Usage: /xban_ignore <player_or_ip>"
+		end
+		local ok, e = xban.whitelist_player(plname, name)
+		return ok, ok  and ("Player %s is now in the whitelist."):format(plname) or e
+	end
+})
+
+minetest.register_chatcommand("xban_unignore", {
+	description = "Remove a player from the ban whitelist",
+	params = "<player_or_ip>",
+	privs = { ban=true },
+	func = function(name, params)
+		local plname = params:match("%S+")
+		if not plname then
+			return false, "Usage: /xban_ignore <player_or_ip>"
+		end
+		local ok, e = xban.unwhitelist_player(plname, name)
+		return ok, ok  and ("Player %s is no longer in the whitelist."):format(plname) or e
+	end
 })
 
 local function check_temp_bans()
